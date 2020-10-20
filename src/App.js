edb8f36f80ec00e4d634c1d5d57d4f47792cfd78
chandanbhardwaj <ckbbarh@gmail.com>
2020-10-20 19:30:12 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    "Milk", "Bread", "Butter"
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      
+      <InputText handleSubmit={(item) => {
+          setItems(items.concat(item));
+        }}
+      />
+       <ListDisplay items={items} 
+       
+       />
     </div>
-  );
+  )
+}
+const onClick=(items)=>{
+  console.log(items)
+}
+
+const ListItem = (props) => (
+  <li  >{props.name}</li>
+)
+ 
+const ListDisplay = (props) => {
+  const items = props.items.map((item, i) => (
+    <ListItem
+      key={i}
+      name={item}
+      handleClick={props.handleClick}
+    />
+  ))
+  return (
+    <ul>
+      {items}
+    </ul>
+  )
+}
+const InputText = (props) => {
+  const [value, setValue] = useState('');
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      props.handleSubmit(value);
+      setValue('');
+    }}>
+    <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
 }
 
 export default App;
