b60ff682b8c259d27295fdc28d92df349164ef9b
chandanbhardwaj <ckbbarh@gmail.com>
2020-10-20 19:35:17 +0530

    Updates

---
+++ precache-manifest.b7db21aee143512feb738b519b7e17c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd38e20ab3c6cde227d53fe7d31b2ef",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "bb106db52b06ea5a34b3",
+    "url": "/shopping-app/static/css/main.48d404bf.chunk.css"
+  },
+  {
+    "revision": "23717ba7e17c956a5fe0",
+    "url": "/shopping-app/static/js/2.792405ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/shopping-app/static/js/2.792405ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb106db52b06ea5a34b3",
+    "url": "/shopping-app/static/js/main.4dfb9bbd.chunk.js"
+  },
+  {
+    "revision": "a4290364a931f3240049",
+    "url": "/shopping-app/static/js/runtime-main.3e77fe51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
